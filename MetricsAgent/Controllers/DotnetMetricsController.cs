c310ac8a9d2de32c4e39eaaa0f45d41ba522abf2
Alexander Serov <zuaz@Yandex.ru>
2022-10-01 12:52:37 +0400

    Выполнено задание к уроку 3.
    Добавлены репозитории, добавлены обработчики в контроллеры.

--- MetricsAgent/Controllers/DotnetMetricsController.cs
+++ MetricsAgent/Controllers/DotnetMetricsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MetricsAgent.Models.Requests;
+using MetricsAgent.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MetricsAgent.Controllers
@@ -7,6 +9,29 @@ namespace MetricsAgent.Controllers
     [ApiController]
     public class DotnetMetricsController : ControllerBase
     {
+
+        private readonly ILogger<DotnetMetricsController> _logger;
+        private readonly IDotnetMetricsRepository _dotnetMetricsRepository;
+
+        public DotnetMetricsController(IDotnetMetricsRepository dotnetMetricsRepository,
+            ILogger<DotnetMetricsController> logger)
+        {
+            _dotnetMetricsRepository = dotnetMetricsRepository;
+            _logger = logger;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] DotnetMetricCreateRequest request)
+        {
+            _logger.LogInformation("Create cpu metric.");
+            _dotnetMetricsRepository.Create(new Models.DotnetMetric
+            {
+                Value = request.Value,
+                Time = (long)request.Time.TotalSeconds
+            });
+            return Ok();
+        }
+
         [HttpGet("errors-count/from/{fromTime}/to/{toTime}")]
         public IActionResult GetErrorsCountMetrics([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
